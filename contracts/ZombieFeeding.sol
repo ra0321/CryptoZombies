ed7cf6e0b8edbd081dad6ee04bd9228c606bcecc
AlphanetRA <alphanetra0321@gmail.com>
2021-12-11 02:15:39 -0800

    Lesson two - Chapter ten

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -2,6 +2,21 @@ pragma solidity >=0.50 <0.6.0;
 
 import "./ZombieFactory.sol";
 
+contract KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    );
+}
+
 contract ZombieFeeding is ZombieFactory {
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
