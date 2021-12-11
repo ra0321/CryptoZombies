58d3b881620f8c7b363749999fd975c5a02e0f85
AlphanetRA <alphanetra0321@gmail.com>
2021-12-10 18:19:44 -0800

    Lesson one - Chapter 8

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -13,7 +13,7 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function createZombie(string memory _name, uint _dna) {
-        
+        zombies.push(Zombie(_name, _dna));
     }
 
     //start here
