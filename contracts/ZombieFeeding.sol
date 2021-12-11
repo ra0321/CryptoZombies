bfc2c148a34c4d59d65fb1b42962f765a8be448c
AlphanetRA <alphanetra0321@gmail.com>
2021-12-11 13:07:12 -0800

    Lesson three - Chapter one

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -19,8 +19,11 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-    KittyInterface kittyContract = KittyInterface(ckAddress);
+    KittyInterface kittyContract;
+
+    function setKittyContractAddress(address _address) external {
+        kittyContract = KittyInterface(_address);
+    }
 
     function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
