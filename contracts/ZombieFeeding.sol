8994fad485736bd2fbc254e6d69033c105f0f3bf
AlphanetRA <alphanetra0321@gmail.com>
2021-12-11 02:21:19 -0800

    Lesson two - Chapter eleven

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -19,6 +19,9 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
