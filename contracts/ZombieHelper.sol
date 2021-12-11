593c9ec4992f9ef49e3f05ad1b95944e9470dcd3
AlphanetRA <alphanetra0321@gmail.com>
2021-12-11 15:51:55 -0800

    Lesson three - Chapter nine

--- contracts/ZombieHelper.sol
+++ contracts/ZombieHelper.sol
@@ -9,4 +9,14 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
+
 }
