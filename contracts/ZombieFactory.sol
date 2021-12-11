1c082f32a7e6f0994b6bd72e7875ddb01e8ff6f2
AlphanetRA <alphanetra0321@gmail.com>
2021-12-10 19:11:33 -0800

    Lesson one - Chapter 13

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.50 <0.6.0;
 
 contract ZombieFactory {
 
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -13,7 +15,8 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
