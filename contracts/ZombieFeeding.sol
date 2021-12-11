0c30f2b37b54b378e4bc04154d29a9402869c4b2
AlphanetRA <alphanetra0321@gmail.com>
2021-12-11 01:48:14 -0800

    Lesson two - Chapter seven

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -3,5 +3,10 @@ pragma solidity >=0.50 <0.6.0;
 import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+    }
     
 }
