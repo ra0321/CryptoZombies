d0061d19d7eabff379f13cde10d96c0266b0f248
AlphanetRA <alphanetra0321@gmail.com>
2021-12-10 17:43:50 -0800

    Lesson one - Chapter 5

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -5,5 +5,11 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
     //start here
+    
 }
