4de6923f150c0c1125b64f6b468434034b1d275d
AlphanetRA <alphanetra0321@gmail.com>
2021-12-10 21:00:57 -0800

    Lesson two - Chapter two

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -14,6 +14,9 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         emit NewZombie(id, _name, _dna);
