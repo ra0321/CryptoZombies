1e549d59aa3d67466ee9a25cdf4f1202b297452f
AlphanetRA <alphanetra0321@gmail.com>
2021-12-11 16:15:47 -0800

    Lesson three - Chapter eleven

--- contracts/ZombieHelper.sol
+++ contracts/ZombieHelper.sol
@@ -20,7 +20,8 @@ contract ZombieHelper is ZombieFeeding {
   }
 
   function getZombiesByOwner(address _owner) external view returns (uint[] memory) {
-    
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    return result;
   }
 
 }
