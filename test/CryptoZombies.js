9ed4d3cbeccae7d4ee8ecce84917676e2ca19cc5
AlphanetRA <alphanetra0321@gmail.com>
2021-12-12 15:55:44 -0800

    Lesson 11 Chapter 6

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -3,12 +3,19 @@ const zombieNames = ["Zombie 1", "Zombie 2"]
 
 contract("CryptoZombies", (accounts) => {
 	let [alice, bob] = accounts
+	let contractInstance
+
+	beforeEach(async () => {
+		contractInstance = await CryptoZombies.new()	// setup
+	})
+
 	it("should be able to create a new zombie", async () => {
-		const contractInstance = await CryptoZombies.new()	// setup
 		const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice})	// act
 
 		// assert
 		assert.equal(result.receipt.status, true)
 		assert.equal(result.logs[0].args.name, zombieNames[0])
 	})
+
+	it("should not allow two zombies", async () => {})
 })
