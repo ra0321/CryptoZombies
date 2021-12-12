4c67c412c6b4f46617b5de82958e5dbdc460c816
AlphanetRA <alphanetra0321@gmail.com>
2021-12-12 05:31:55 -0800

    Lesson five - Chapter seven

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -28,7 +28,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         _transfer(_from, _to, _tokenId);
     }
 
-    function approve(address _approved, uint256 _tokenId) external payable {
-
+    function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+        zombieApprovals[_tokenId] = _approved;
     }
 }
