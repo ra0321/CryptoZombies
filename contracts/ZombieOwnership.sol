4fd385841acb9abc7f272380217b1bd12a5a1b5d
AlphanetRA <alphanetra0321@gmail.com>
2021-12-12 04:57:32 -0800

    Lesson five - Chapter two

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -1,7 +1,8 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./ZombieAttack.sol";
+import "./ERC721.sol";
 
-contract ZombieOwnership is ZombieAttack {
+contract ZombieOwnership is ZombieAttack, ERC721 {
     
 }
