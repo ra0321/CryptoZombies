d8f60b9c2887dd07e0cc5befe56c05188f96ad2f
AlphanetRA <alphanetra0321@gmail.com>
2021-12-12 05:02:41 -0800

    Lesson five - Chapter three

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -5,4 +5,19 @@ import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
     
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerZombieCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        return zombieToOwner[_tokenId];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+
+    }
+
+    function approve(address _approved, uint256 _tokenId) external payable {
+
+    }
 }
