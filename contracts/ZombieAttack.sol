8fae185aebd826e2dcf3835285e690f6aa754183
AlphanetRA <alphanetra0321@gmail.com>
2021-12-12 02:37:28 -0800

    Lesson four - Chapter five

--- contracts/ZombieAttack.sol
+++ contracts/ZombieAttack.sol
@@ -3,11 +3,16 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./ZombieHelper.sol";
 
 contract ZombieAttack is ZombieHelper {
-    
+
     uint randNonce = 0;
+    uint attackVictoryProbability = 70;
 
     function randMod(uint _modulus) internal returns (uint) {
         randNonce++;
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+    }
+
+    function attack(uint _zombieId, uint _targetId) external {
+        
     }
 }
