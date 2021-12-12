aa749a54421c64a0730b9eefdf69b8f2c526d441
AlphanetRA <alphanetra0321@gmail.com>
2021-12-12 05:27:27 -0800

    Lesson five - Chapter six

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -4,6 +4,8 @@ import "./ZombieAttack.sol";
 import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+
+    mapping (uint => address) zombieApprovals;
     
     function balanceOf(address _owner) external view returns (uint256) {
         return ownerZombieCount[_owner];
@@ -22,7 +24,8 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     }
 
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-
+        require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
     }
 
     function approve(address _approved, uint256 _tokenId) external payable {
